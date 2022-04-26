c6bd32e2014f78b4be0f493076f5ded57f580ee2
b1018043 <grey.fox.0417@gmail.com>
2022-04-26 22:29:11 +0900

    chore:add storybook

---
+++ components/linkcard.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { useState, useRef } from 'react';
+
+const LinkCard: React.FunctionComponent = ()=>{
+    return (
+        <></>
+    )
+}
+
+export default LinkCard;
