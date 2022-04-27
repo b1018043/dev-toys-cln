f557800a05a6ac786304b060e25cfd7ea8b5c82b
b1018043 <grey.fox.0417@gmail.com>
2022-04-27 22:38:31 +0900

    fix:add styling minheader

--- components/atoms/minheader.tsx
+++ components/atoms/minheader.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 type MinHeaderProps = React.DetailedHTMLProps<React.HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement>;
 
 const MinHeader: React.FunctionComponent<MinHeaderProps> = (props) =>{
-    return <h6 className='text-gray-200 text-5xl' children={props.children}/>
+    return <h6 className='text-gray-200 text-xl' children={props.children}/>
 }
 
 export default MinHeader;
